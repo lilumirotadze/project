bc3b82afa8946ae299af7ad425c9e1a20e42fc50
lilu mirotadze <mirotadzelilu@yahoo.com>
2022-01-26 00:14:45 +0400

    full

---
+++ script.js
@@ -0,0 +1,8 @@
+/* burger code */
+
+let chvens = document.getElementById ("navigationBlock");
+let knopka = document.getElementById ("BurgerIcon");
+
+knopka.addEventListener("click",function() { 
+    chvens.classList.toggle("igini");
+})
